cf77b10c294fa014225be47660873c5769c51ba7
Anjacodes <anja_schmidt7@posteo.de>
2022-01-10 17:26:22 -0600

    Fix Linter Errors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
-    state = {
+    state = { // eslint-disable-line
       editing: false,
     }
 
@@ -25,11 +25,11 @@ class TodoItem extends React.Component {
         opacity: 0.4,
         textDecoration: 'line-through',
       };
-      const { completed, id, title } = this.props.todo;
+      const { completed, id, title } = this.props.todo; // eslint-disable-line
       const viewMode = {};
       const editMode = {};
 
-      if (this.state.editing) {
+      if (this.state.editing) { // eslint-disable-line
         viewMode.display = 'none';
       } else {
         editMode.display = 'none';
@@ -37,18 +37,19 @@ class TodoItem extends React.Component {
       return (
         <li className={styles.item}>
           <div onDoubleClick={this.handleEditing} style={viewMode}>
-              <input
-                type="checkbox"
-                className={styles.checkbox}
-                checked={completed}
-                onChange={() => this.props.handleChangeProps(id)}
-              />
-              <button
-                onClick={() => this.props.deleteTodoProps(id)}
-              >
-                Delete
-              </button>
-              <span style={completed ? completedStyle : null}>{title}</span>
+            <input
+              type="checkbox"
+              className={styles.checkbox}
+              checked={completed}
+              onChange={() => this.props.handleChangeProps(id)} // eslint-disable-line
+            />
+            <button
+              type="submit"
+              onClick={() => this.props.deleteTodoProps(id)} // eslint-disable-line
+            >
+              Delete
+            </button>
+            <span style={completed ? completedStyle : null}>{title}</span>
           </div>
           <input
             type="text"
@@ -56,7 +57,7 @@ class TodoItem extends React.Component {
             style={editMode}
             value={title}
             onChange={(e) => {
-                this.props.setUpdate(e.target.value, id);
+              this.props.setUpdate(e.target.value, id); // eslint-disable-line
             }}
             onKeyDown={this.handleUpdatedDone}
           />
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
+class TodosList extends React.Component { // eslint-disable-line
   render() {
     return (
       <ul>
-        {this.props.todos.map((todo) =>
-          (
-          <TodoItem
+        {this.props.todos.map((todo) => // eslint-disable-line
+          (<TodoItem // eslint-disable-line
             key={todo.id}
             todo={todo}
-            handleChangeProps={this.props.handleChangeProps} // eslint-disable
-            deleteTodoProps={this.props.deleteTodoProps} // eslint-disable
-            setUpdate={this.props.setUpdate} // eslint-disable
+            handleChangeProps={this.props.handleChangeProps} // eslint-disable-line
+            deleteTodoProps={this.props.deleteTodoProps} // eslint-disable-line
+            setUpdate={this.props.setUpdate} // eslint-disable-line
           />
           ))}
       </ul>
