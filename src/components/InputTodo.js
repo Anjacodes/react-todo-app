064986cda9e8e04b21edb0a480c5fe88ac0f0e3d
Anjacodes <anja_schmidt7@posteo.de>
2022-01-10 13:14:01 -0600

    Build App

---
+++ src/components/InputTodo.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if(this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: ""
+      });
+    } else {
+      alert("Please write task")
+    }
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+        name="title" type="text"
+        className="input-text"
+        placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
+        <button className="input-submit">Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
